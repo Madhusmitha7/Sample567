21560946308411ae4e578726869a1f77aac31215
Madhusmitha7 <konda.madhusmitha@gmail.com>
2022-12-22 10:51:21 +0530

    My commit

---
+++ Bhavasri1234/src/main/java/Bhavasri1234/Bhavasri1234/App.java
@@ -0,0 +1,13 @@
+package Bhavasri1234.Bhavasri1234;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
